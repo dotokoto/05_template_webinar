3f6fafbf2419b5d365547ee434c48893624af1cd
Natalia Druzhinina <dotokoto@gmail.com>
2022-12-22 22:02:27 +0500

    refactor: card templates

--- script.js
+++ script.js
@@ -6,27 +6,23 @@ const items = [
 ];
 
 const cardsContainer = document.querySelector('.list');
+const cardTemplate = document
+	.querySelector('.item-template')
+	.content
+	.querySelector('.list__item');
 
 function createCard(text) {
-	const card = `
-<li class="list__item">
-	<span class="item__text">${text}</span>
-	<img class="edit" src="images/Edit.png" alt="Редактировать" />
-	<img
-	class="duplicate"
-	src="images/Duplicate.png"
-	alt="Копировать"
-	/>
-	<img class="delete" src="images/Delete.png" alt="Удалить" />
-</li>
-`;
+	const card = cardTemplate.cloneNode(true);
+	const cardText = card.querySelector('.item__text');
+	cardText.textContent = text;
+
 	return card;
 }
 
 function renderCards() {
 	items.forEach(item => {
 		const cardHtml = createCard(item);
-		cardsContainer.innerHTML += cardHtml;
+		cardsContainer.append(cardHtml);
 	});
 }
 
