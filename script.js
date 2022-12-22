816ae490b9be7322d0aa3841824b9fd3a92a8f97
Natalia Druzhinina <dotokoto@gmail.com>
2022-12-22 21:31:52 +0500

    feat: render cards

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const items = [
 	"Пройти туториал по Реакту",
 	"Сделать фронт для своего проекта",
 ];
+
+const cardsContainer = document.querySelector('.list');
+
+function createCard(text) {
+	const card = `
+<li class="list__item">
+	<span class="item__text">${text}</span>
+	<img class="edit" src="images/Edit.png" alt="Редактировать" />
+	<img
+	class="duplicate"
+	src="images/Duplicate.png"
+	alt="Копировать"
+	/>
+	<img class="delete" src="images/Delete.png" alt="Удалить" />
+</li>
+`;
+	return card;
+}
+
+function renderCards() {
+	items.forEach(item => {
+		const cardHtml = createCard(item);
+		cardsContainer.innerHTML += cardHtml;
+	});
+}
+
+renderCards();
