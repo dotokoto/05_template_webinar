b9ae376f676ba4b077bad415de31fc22587248f6
Natalia Druzhinina <dotokoto@gmail.com>
2022-12-22 22:13:23 +0500

    feat: add new card

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const cardTemplate = document
 	.content
 	.querySelector('.list__item');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.form__input');
+
 function createCard(text) {
 	const card = cardTemplate.cloneNode(true);
 	const cardText = card.querySelector('.item__text');
@@ -19,11 +22,26 @@ function createCard(text) {
 	return card;
 }
 
+function addCard(card) {
+	cardsContainer.prepend(card);
+}
+
 function renderCards() {
-	items.forEach(item => {
+	items.reverse().forEach(item => {
 		const cardHtml = createCard(item);
-		cardsContainer.append(cardHtml);
+		addCard(cardHtml);
 	});
 }
+
+
+function submitForm(event) {
+	event.preventDefault();
+
+	const newCard = createCard(input.value);
+	addCard(newCard);
+}
+
+
+form.addEventListener('submit', submitForm);
 
 renderCards();
